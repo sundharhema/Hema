9d438de1103c89d354d7a24ae6e6c75e4255cc9c
sundharhema <41180483+sundharhema@users.noreply.github.com>
2018-07-13 11:31:16 +0530

    aa.java

---
+++ aa.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class aa
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        
+            if(n>0)
+            {
+                System.out.print("POSITIVE");
+            }
+            else if(n<0)
+            {
+                System.out.print("NEGATIVE");
+            }
+            else
+            {
+                System.out.print("ZERO");
+            }
+        
+    }
+}
