6fd05dec2e23d657948806a39aff7d87cdc65b45
sundharhema <41180483+sundharhema@users.noreply.github.com>
2018-07-17 12:14:56 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class rev
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        for(int i=a.length()-1;i>=0;i--)
+        {
+            System.out.print(a.charAt(i));
+        }
+    }
+}
