1a6ea14f4807cc143c19f82feca1ee943e5141fb
sundharhema <41180483+sundharhema@users.noreply.github.com>
2018-07-17 12:25:24 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class app
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        System.out.print(a+".");
+    }
+}
